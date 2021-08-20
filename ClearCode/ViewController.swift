bc03ba001d5617bb03e7e0cfd5f425116173a51c
Ivan3RG <ivan.ko4etkov@yandex.ru>
2021-08-20 18:56:56 +0300

    init

---
+++ ClearCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClearCode
+//
+//  Created by Иван Кочетков on 20.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
